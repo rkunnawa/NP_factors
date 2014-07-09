605d58b92a6f2a0e6583e57e297ece9f6893e3f0
Raghav Kunnawalkam Elayavalli <raghav.k.e@cern.ch>
2014-07-09 19:02:50 +0200

    update july9 working setup for 2760 and 5020

--- test/analysis.C
+++ test/analysis.C
@@ -44,7 +44,10 @@
 using namespace std;
 
 
-void analysis(int radius= 3, int energy = 2760){
+static const int dir = 50;
+
+
+void analysis(int energy = 5020){
 
 	TStopwatch timer;
 	timer.Start();
@@ -57,15 +60,14 @@ void analysis(int radius= 3, int energy = 2760){
 	TH2::SetDefaultSumw2();
 
 	cout<<"Starting the calculation of the NP correction facrors"<<endl;
-	cout<<"Running for Radius = "<<radius<<" and Energy = "<<energy<<endl;
+	cout<<"Running for Energy = "<<energy<<endl;
 
 	
-	TFile *fin_LO = TFile::Open(Form("rootfiles/GenJet_LO_R23457_%dGeV_July8_Z2Combined.root",energy));
-	TFile *fin_HAD = TFile::Open(Form("rootfiles/GenJet_HAD_R23457_%dGeV_July8_Z2Combined.root",energy));
+	TFile *fin_LO = TFile::Open(Form("rootfiles/GenJet_LO_R23457_%dGeV_July9_Z2Combined.root",energy));
+	TFile *fin_HAD = TFile::Open(Form("rootfiles/GenJet_HAD_R23457_%dGeV_July9_Z2Combined.root",energy));
 
 	//get the histograms required. there are a lot here: 
 
-	const int dir = 50;
 
 	char dirName[dir][256] = {"ak3GenJetSpectrum_n10_p10","ak3GenJetSpectrum_n20_p20","ak3GenJetSpectrum_n25_n20","ak3GenJetSpectrum_n20_n15",
     "ak3GenJetSpectrum_n15_n10","ak3GenJetSpectrum_n10_n05","ak3GenJetSpectrum_n05_p05","ak3GenJetSpectrum_p05_p10",
@@ -83,22 +85,28 @@ void analysis(int radius= 3, int energy = 2760){
     "ak7GenJetSpectrum_n15_n10","ak7GenJetSpectrum_n10_n05","ak7GenJetSpectrum_n05_p05","ak7GenJetSpectrum_p05_p10",
     "ak7GenJetSpectrum_p10_p15","ak7GenJetSpectrum_p15_p20"};
 
-    char etaWidth[dir][256] = {"-1.0<#eta<1.0","-2.0<#eta<2.0","-2.5<#eta<2.0","-2.0<#eta<-1.5",
-	"-1.5<#eta<1.0","-1.0<#eta<-0.5","-0.5<eta<0.5","0.5<#eta<1.0",
-	"1.0<#eta<1.5","1.5<#eta<2.0",
-	"-1.0<#eta<1.0","-2.0<#eta<2.0","-2.5<#eta<2.0","-2.0<#eta<-1.5",
-	"-1.5<#eta<1.0","-1.0<#eta<-0.5","-0.5<eta<0.5","0.5<#eta<1.0",
-	"1.0<#eta<1.5","1.5<#eta<2.0",
-	"-1.0<#eta<1.0","-2.0<#eta<2.0","-2.5<#eta<2.0","-2.0<#eta<-1.5",
-	"-1.5<#eta<1.0","-1.0<#eta<-0.5","-0.5<eta<0.5","0.5<#eta<1.0",
-	"1.0<#eta<1.5","1.5<#eta<2.0",
-	"-1.0<#eta<1.0","-2.0<#eta<2.0","-2.5<#eta<2.0","-2.0<#eta<-1.5",
-	"-1.5<#eta<1.0","-1.0<#eta<-0.5","-0.5<eta<0.5","0.5<#eta<1.0",
-	"1.0<#eta<1.5","1.5<#eta<2.0",
-	"-1.0<#eta<1.0","-2.0<#eta<2.0","-2.5<#eta<2.0","-2.0<#eta<-1.5",
-	"-1.5<#eta<1.0","-1.0<#eta<-0.5","-0.5<eta<0.5","0.5<#eta<1.0",
-	"1.0<#eta<1.5","1.5<#eta<2.0"};
+    char etaWidth[dir][256] = {"n10_eta_p10","n20_eta_p20","n25_eta_n20","n20_eta_n15",
+	"n15_eta_n10","n10_eta_n05","n05_eta_p05","p05_eta_p10",
+	"p10_eta_p15","p15_eta_p20",
+	"n10_eta_p10","n20_eta_p20","n25_eta_n20","n20_eta_n15",
+	"n15_eta_n10","n10_eta_n05","n05_eta_p05","p05_eta_p10",
+	"p10_eta_p15","p15_eta_p20",
+	"n10_eta_p10","n20_eta_p20","n25_eta_n20","n20_eta_n15",
+	"n15_eta_n10","n10_eta_n05","n05_eta_p05","p05_eta_p10",
+	"p10_eta_p15","p15_eta_p20",
+	"n10_eta_p10","n20_eta_p20","n25_eta_n20","n20_eta_n15",
+	"n15_eta_n10","n10_eta_n05","n05_eta_p05","p05_eta_p10",
+	"p10_eta_p15","p15_eta_p20",
+	"n10_eta_p10","n20_eta_p20","n25_eta_n20","n20_eta_n15",
+	"n15_eta_n10","n10_eta_n05","n05_eta_p05","p05_eta_p10",
+	"p10_eta_p15","p15_eta_p20"
+	};
 
+	char radius_lable[dir][256] = {"R3","R3","R3","R3","R3","R3","R3","R3","R3","R3",
+	"R2","R2","R2","R2","R2","R2","R2","R2","R2","R2",
+	"R4","R4","R4","R4","R4","R4","R4","R4","R4","R4",
+	"R5","R5","R5","R5","R5","R5","R5","R5","R5","R5",
+	"R7","R7","R7","R7","R7","R7","R7","R7","R7","R7"};
 
 	// R=0.3 is from dirName 0-9
 	// R=0.2 is from dirName 10-19
@@ -107,23 +115,70 @@ void analysis(int radius= 3, int energy = 2760){
 	// R=0.7 is from dirName 40-49
 
     TH1F *LO_spectra[dir];
-    TH1F *LO_spectraFine[dir]
+    TH1F *LO_spectraFine[dir];
     TH1F *HAD_spectra[dir];
     TH1F *HAD_spectraFine[dir];
+
+    TH1F *NPC[dir];
+    TH1F *NPC_fine[dir];
 
     for(int i = 0;i<dir;i++){
 
     	LO_spectra[i] = (TH1F*)fin_LO->Get(Form("%s/JetSpectrum",dirName[i]));
     	LO_spectraFine[i] = (TH1F*)fin_LO->Get(Form("%s/JetSpectrum_Fine",dirName[i]));
-    	HAD_Spectra[i] = (TH1F*)fin_HAD->Get(Form("%s/JetSpectrum",dirName[i]));
-    	HAD_SpectraFine[i] = (TH1F*)fin_HAD->Get(Form("%s/JetSpectrum_Fine",dirName[i]));
+    	HAD_spectra[i] = (TH1F*)fin_HAD->Get(Form("%s/JetSpectrum",dirName[i]));
+    	HAD_spectraFine[i] = (TH1F*)fin_HAD->Get(Form("%s/JetSpectrum_Fine",dirName[i]));
+
+    	NPC[i] = (TH1F*)LO_spectra[i]->Clone(Form("LO_NPC_%s",dirName[i]));
+    	NPC[i]->Divide(HAD_spectra[i]);
 
     }
 
-    // lets first plot the spectra in a power law fit and see how it looks. 
+    // one for each radius
+    TCanvas *cSpectraCheck[5];
 
-    //Get the NP factors which are per bin. and then throw them into the text files. 
+    char radius_title[5][256] = {"R3","R2","R4","R5","R7"};
 
+    TLine *line = new TLine(50,1,300,1);
+    line->SetLineStyle(2);
+    line->SetLineWidth(2);
+
+    for(int i = 1;i<=5;i++){
+
+    	cSpectraCheck[i] = new TCanvas(Form("cSpectraCheck_%d",i),Form("NP correction factors for ak %s GenJet with energy %d",radius_title[i-1],energy),1200,800);
+    	cSpectraCheck[i]->Divide(5,2);
+
+    	for(int j = 1;j<=10;j++){
+
+    		cSpectraCheck[i]->cd(j);
+
+    		drawText(Form("%s_%s",etaWidth[(i-1)*10+j-1],radius_title[i]),0.1,0.9,20);
+    		line->Draw();
+    		NPC[(i-1)*10+j-1]->SetMarkerStyle(33);
+    		NPC[(i-1)*10+j-1]->SetMarkerColor(kBlack);
+    		NPC[(i-1)*10+j-1]->SetTitle(Form("%s %s",etaWidth[(i-1)*10+j-1],radius_title[i-1]));
+    		NPC[(i-1)*10+j-1]->SetXTitle("Jet p_{T} (GeV/c)");
+    		NPC[(i-1)*10+j-1]->SetYTitle("correction factor");
+    		NPC[(i-1)*10+j-1]->Draw();
+    	}
+
+    	cSpectraCheck[i]->SaveAs(Form("/afs/cern.ch/work/r/rkunnawa/WORK/RAA/pp_genjets_pythia_NPC/CMSSW_5_3_8_HI_patch2/src/Appeltel/GenJetCrossCheckAnalyzer/test/plots/NPC_factors_ak_%s_energy_%d.pdf",radius_title[i-1],energy),"RECREATE");
+
+    }
+
+    //create the text files. 
+    ofstream NPC_txt[dir];
+    for(int i = 0;i<dir;i++){
+    	NPC_txt[i].open(Form("txtfiles/NPC_ak_%s_%s_energy%d.txt",radius_lable[i],etaWidth[i],energy));
+
+    	for(int j = 0;j<NPC[0]->GetNbinsX();j++){
+    		float bincenter = NPC[i]->GetBinCenter(j);
+    		NPC_txt[i]<<bincenter<<" "<<NPC[i]->GetBinContent(j)<<endl;
+    	}
+
+    	//NPC_txt.Close();
+
+    }
 
 
 
--- test/makeCombinedPtHatSample357.C
+++ test/makeCombinedPtHatSample357.C
@@ -1,7 +1,7 @@
 {
 
-  char energy[256] = "2760";
-
+  char energy[256] = "5020";
+  char order[256] = "HAD";
   //const int MAXDIR = 61;
   const int MAXDIR = 50;
 
@@ -19,7 +19,10 @@
   //double xsec[12] = {0.7966,0.19265,0.009725,0.00092635,8.736e-05,9.825e-06,1.1813e-06,2.3047e-07,4.7296e-08,5.1215e-09,6.658e-10,1.467e-10};
   // following is added to check for the initial pthat process 
   //double xsec[12] = {2.034e-01,0.19265,0.009725,0.00092635,8.736e-05,9.825e-06,1.1813e-06,2.3047e-07,4.7296e-08,5.1215e-09,6.658e-10,1.467e-10};
-  double xsec[12] = {61.77,0.19265,0.009725,0.00092635,8.736e-05,9.825e-06,1.1813e-06,2.3047e-07,4.7296e-08,5.1215e-09,6.658e-10,1.467e-10};
+  //double xsec[12] = {61.77,0.19265,0.009725,0.00092635,8.736e-05,9.825e-06,1.1813e-06,2.3047e-07,4.7296e-08,5.1215e-09,6.658e-10,1.467e-10};
+
+  //getting it from the log files 
+  //double xsec[12] = {6.198e01,1.921e-01,9.771e-03,9.253e-04,8.793e-05,9.766e-06,1.185e-06,2.310e-07,4.721e-08,5.128e-09,6.647e-10,1.483e-10};
 
   // for 5.02 TeV
   //double xsec[10] = {6.782e+01, 1.405e-01, 2.995e-02, 3.334e-03,
@@ -28,6 +31,10 @@
   //5.02 new, the last value here is wrong. make sure of that. and this doesnt have pthat value 540. remove that when running this for 5.02TeV  
   // double xsec[10] = {5.335e-01,3.378E-02,3.778E-03,4.412E-04,6.147E-05,1.018E-05,2.477E-06,6.160E-07,1.088E-07,2.527E-08,1.467E-10};
   // double xsec[11] = {1.8695,0.49972,0.030002,0.0033368,0.00037973,5.129e-05,7.703e-06,1.861e-06,5.072e-07,8.353e-08,2.527e-08};
+  // getting the cross section from the log files 
+  double xsec[12] = {6.782e01,4.988e-01,2.988e-02,3.329e-03,3.801e-04,5.183e-05,7.695e-06,1.863e-06,5.051e-07,8.381e-08,1.727e-08,7.847e-09};
+
+
 
   // for 7 TeV
   //double xsec[10] = {7.131e+01, 2.359e-01, 5.307e-02, 6.354e-03,
@@ -81,7 +88,7 @@
   for( int i=0; i<12;i++)
   {
     //do it once for HAD and once for LO
-    f[i] = new TFile(Form("rootfiles/GenJet_HAD_R23457_%s_July8_Z2pt%s_numEvent100000.root",energy,rpthats[i]));
+    f[i] = new TFile(Form("rootfiles/GenJet_%s_R23457_%s_July8_Z2pt%s_numEvent100000.root",order,energy,rpthats[i]));
     for( int j=0; j<MAXDIR; j++)
     {
       events[i][j] = (TH1F *) f[i]->Get(Form("%s/events",dirname[j]));
@@ -120,7 +127,7 @@
   TH1F * spectrum_out[MAXDIR];
   TH1F * spectrumf_out[MAXDIR];
 
-  TFile * f_out = new TFile(Form("rootfiles/GenJet_HAD_R23457_%sGeV_July8_Z2Combined.root",energy),"RECREATE");
+  TFile * f_out = new TFile(Form("rootfiles/GenJet_%s_R23457_%sGeV_July9_Z2Combined.root",order,energy),"RECREATE");
 
   for( int j=0;j<MAXDIR;j++)
   {
